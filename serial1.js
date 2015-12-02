b8fb99ae6f0b1680207be446515440d65c73f811
Peter Kisfaludi <pkissfa@gmail.com>
2015-12-02 12:16:29 +0100

    .

--- serial1.js
+++ serial1.js
@@ -4,7 +4,7 @@
 //
 // This is an extension for development and testing of the Scratch Javascript Extension API.
 
-
+////////////////////////////////////////////
 //WebSocket client
 console.log('hello from scratchx extension');
 if ('WebSocket' in window){
@@ -21,12 +21,14 @@ ws.onopen = function() {
 ws.onmessage = function(e){
    var server_message = e.data;
    console.log(server_message);
+   ext.onserial(server_message);
 };
 
+////////////////////////////////////////////
 //scratch extension
 (function (ext) {
     var device = null;
-    var rawData = null;
+    var serialReceived = false;
 
     // Sensor states:
     var channels = {
@@ -43,121 +45,38 @@ ws.onmessage = function(e){
         console.log('ext.sensor called with ' + which);
         return getSensor(which);
     };
+    
+    ext.SR = function() {
+        var ret = serialReceived;
+        return ret;
+    };
 
     // Private logic
-
     function getSensor(which) {
         console.log('get sensor called with ' + which);
         return inputs[which];
     }
+    
+    ext.onserial = function (raw) {
+        console.log('extension received: '+raw);
+    }
 
     var inputArray = [];
 
-    function processData() {
-        console.log('processing data');
-        var bytes = new Uint8Array(rawData);
-
-        inputArray[15] = 0;
-
-        for (var name in inputs) {
-            var v = inputArray[channels[name]];
-            if (name == 'A') {
-                v=100;
-            }
-            else if (name == 'B') {
-                v=200;
-            }
-            else {
-                v = 300;
-            }
-
-            inputs[name] = v;
-        }
-
-        rawData = null;
-    }
-
-    function appendBuffer(buffer1, buffer2) {
-        console.log('append buffer called');
-        var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-        tmp.set(new Uint8Array(buffer1), 0);
-        tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
-        return tmp.buffer;
-    }
-
-    // Extension API interactions
-    var potentialDevices = [];
-    ext._deviceConnected = function (dev) {
-        console.log('_deviceConnected called' + dev);
-        potentialDevices.push(dev);
-
-        if (!device) {
-            tryNextDevice();
-        }
-    };
-
-    function tryNextDevice() {
-        // If potentialDevices is empty, device will be undefined.
-        // That will get us back here next time a device is connected.
-        device = potentialDevices.shift();
-        console.log('tryNext called, device is ' + device);
-
-        if (device) {
-            console.log('serial port opened');
-            device.open({stopBits: 0, bitRate: 115200, ctsFlowControl: 0}, deviceOpened);
-        }
-    }
-
-    function deviceOpened(dev) {
-        console.log('DeviceOpened' + dev);
-        if (!dev) {
-            // Opening the port failed.
-            tryNextDevice();
-            return;
-        }
-        device.set_receive_handler(function (data) {
-            console.log('Received: ' + data.byteLength);
-            if (!rawData || rawData.byteLength == 18) {
-                rawData = new Uint8Array(data);
-            } else {
-                rawData = appendBuffer(rawData, data);
-            }
-
-            if (rawData.byteLength >= 18) {
-                console.log(rawData);
-                processData();
-            }
-        });
-    }
-
-    ext._deviceRemoved = function (dev) {
-        console.log('_deviceRemoved');
-        if (device != dev) return;
-        device = null;
-    };
-
-    ext._shutdown = function () {
-        if (device) device.close();
-        device = null;
-    };
+    ext._shutdown = function () {};
 
     ext._getStatus = function () {
-        console.log('getStatus called');
-        if (!device) {
-            console.log('status = 1');
-            return {status: 1, msg: 'PicoBoard disconnected'};
-        }
-        console.log('status = 2');
-        return {status: 2, msg: 'PicoBoard connected'};
+        return {status: 2, msg: 'Ready'};
     };
 
     var descriptor = {
         blocks: [
-            ['r', '%m.sensor sensor value',        'sensor',              'A']
+            ['r', '%m.sensor value',        'sensor',              'A']
+//            ['r', 'Serial Received',        'SR',                     ]
         ],
         menus: {
             sensor: ['A', 'B'],
         },
     };
-    ScratchExtensions.register('Serial1', descriptor, ext, {type: 'serial'});
+    ScratchExtensions.register('Serial1', descriptor, ext);
 })({});
